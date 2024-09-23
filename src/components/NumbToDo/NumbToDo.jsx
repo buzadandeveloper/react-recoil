3de5cf93cd4a02492a89cebfbbf17b65d8db62f0
Buza Dan <buzdandeveloper@gmail.com>
2024-09-23 14:19:19 +0300

    feat: created componet for total numb of todos

---
+++ src/components/NumbToDo/NumbToDo.jsx
@@ -0,0 +1,13 @@
+import { useRecoilValue } from "recoil";
+import { TotalToDo } from "../../state/selectros/TotalToDo";
+
+const NumbToDo = () => {
+  const totalToDo = useRecoilValue(TotalToDo);
+  return (
+    <div className="bg-white w-[350px] flex justify-center items-center p-9 rounded-xl shadow-lg shadow-gray-500/50 md:w-[500px] h-[75px]">
+      <h3 className="font-bold text-center">Total tasks: {totalToDo}</h3>
+    </div>
+  );
+};
+
+export default NumbToDo;
